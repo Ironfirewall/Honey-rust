80be1b62db74c56124d3d44091dd51ca5d64179e
Ironfirewall <57204380+Ironfirewall@users.noreply.github.com>
2023-02-02 20:18:50 -0500

    project setup

---
+++ honey/target/debug/deps/honey.d
@@ -0,0 +1,5 @@
+C:\Users\Jan\source\repos\Honey\honey\target\debug\deps\honey.exe: src\main.rs
+
+C:\Users\Jan\source\repos\Honey\honey\target\debug\deps\honey.d: src\main.rs
+
+src\main.rs:
---
+++ honey/target/debug/honey.d
@@ -0,0 +1,1 @@
+C:\Users\Jan\source\repos\Honey\honey\target\debug\honey.exe: C:\Users\Jan\source\repos\Honey\honey\src\main.rs
