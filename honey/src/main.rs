148d755cf80c1a4bfc381f898318bf5eb17a8b7b
Ironfirewall <57204380+Ironfirewall@users.noreply.github.com>
2023-02-11 11:13:00 -0500

    add git ignore

--- honey/src/main.rs
+++ honey/src/main.rs
@@ -1,3 +1,28 @@
+//to run main
+//cd to this dir and hit "cargo run"
+
+#![allow(
+    dead_code,
+    unused_variables,
+    clippy::too_many_arguments,
+    clippy::unnecessary_wraps
+)]
+
+use anyhow::Result;
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window,WindowBuilder}
+};
+
 fn main() {
-    println!("Hello, world with cargo!");
+    //println!("Hello, world with cargo!");
+    logger("Begin Program".to_string());
+    logger("Eng Program".to_string());
+}
+
+
+fn logger(message : String){
+    println!("{}", message);
 }
